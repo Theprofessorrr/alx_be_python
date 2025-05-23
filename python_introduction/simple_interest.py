b69f623b10994ee3a0fbaa027074ae8ad0fb7b65
Eric Hervinn <hervinnpo@gmail.com>
2025-05-24 01:52:11 +0200

    Adding simple_interest.py

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
-principal = 1000
-rate = 0.05
-time = 3
+P = 1000
+R = 0.05
+T = 3
 
-i = principal * rate * time
+I = P * R * T
 
-print("The simple interest is:", i)
+print("The simple interest is:", I)
