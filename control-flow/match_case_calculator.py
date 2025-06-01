5b5bffe31aacff96c69d9f9720fbd1a4c59c1b41
Eric Hervinn <hervinnpo@gmail.com>
2025-06-01 19:45:26 +0200

    Add changes to match calculator app

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,24 @@
+# Get user inputs
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+# Match-case for operation
+match operation:
+    case "+":
+        result = num1 + num2
+        print("The result is", result)
+    case "-":
+        result = num1 - num2
+        print("The result is", result)
+    case "*":
+        result = num1 * num2
+        print("The result is", result)
+    case "/":
+        if num2 == 0:
+            print("Error: Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print("The result is", result)
+    case _:
+        print("Invalid operation selected.")
