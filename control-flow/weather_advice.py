611c930a4b682b19672bc46a7b0e13482725a7a5
Eric Hervinn <hervinnpo@gmail.com>
2025-06-01 19:04:02 +0200

    create a new weather app file and commit changes made to it

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather_value = input("What's the weather like today? (sunny/rainy/cold):")
+
+if weather_value == "sunny" :
+    print("Wear a t-shirt and sunglasses.")
+elif weather_value == "rainy" :
+    print("Don't forget your umbrella and a raincoat.")
+elif weather_value == "cold" :
+    print("Make sure to wear a warm coat and a scarf.")
+else :
+    print("Sorry, I don't have recommendations for this weather.")
