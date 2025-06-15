0e6d1d93fdd73065ae44b397198a9d78fc56fa0c
Eric Hervinn <hervinnpo@gmail.com>
2025-06-15 23:42:47 +0200

    changes to simple test file

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
         """Initialize calculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         """Test the add method with various inputs."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, -1), -2)
