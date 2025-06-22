d544df56fa5936eccf94e9857a95095e4a166eed
Eric Hervinn <hervinnpo@gmail.com>
2025-06-22 23:47:00 +0200

    adding changes to files main and library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,16 +5,16 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_info(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size  # in KB
+        self.file_size = file_size
 
-    def get_info(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
@@ -23,7 +23,7 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_info(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -39,4 +39,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_info())
+            print(book)  # This will use the __str__ method of each book
